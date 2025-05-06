34a8c93f89685b68489c6a0ec89ca54fdfee06ae
Borislav Tsvetanov <stu2101261055@uni-plovdiv.bg>
2025-05-06 23:41:25 +0300

    final commit

--- src/features/cards/Card.js
+++ src/features/cards/Card.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
-// import selector
+import { selectCardsId } from "./cardsSlice";
 
 export default function Card({ id }) {
-  const card = {}; // replace this with a call to your selector to get a card by id
+  const cards = useSelector(selectCardsId);
+  const card = cards[id]
   const [flipped, setFlipped] = useState(false);
 
   return (
---
+++ src/features/topics/topicsSlice.js
@@ -0,0 +1,31 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { addQuiz } from "../quizzes/quizzesSlice";
+
+const topicsSlice = createSlice({
+    name: 'topics',
+    initialState: {
+        topics: {}
+    },
+    reducers: {
+        addTopic: (state, action) => {
+            const {id, name, icon } = action.payload;
+            state.topics[id] = {
+                id, 
+                name,
+                icon,
+                quizIds: []
+            }
+        },
+    },
+    extraReducers: {
+        [addQuiz] : (state, action) => {
+            const { topicId, id } = action.payload;
+            state.topics[topicId].quizIds.push(id);
+        }
+    }
+});
+
+export const selectTopics = (state) => state.topics.topics;
+
+export const { addTopic } = topicsSlice.actions;
+export default topicsSlice.reducer;
